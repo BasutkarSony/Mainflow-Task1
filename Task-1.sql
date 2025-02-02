ff44c4e9235c9e82d634be319b32c7b621a824bd
BasutkarSony <basutkarsony14@gmail.com>
2025-02-02 20:14:56 +0530

    Task-1.sql

--- Task-1.sql
+++ Task-1.sql
@@ -1,4 +1,7 @@
+# Creating a Database
 create database studentmanagement;
+
+# Creating a table students
 create table students(
 studentId Integer Primary Key auto_increment Not NULL,
 Name varchar(50),
@@ -9,6 +12,8 @@ MathScore integer,
 ScienceScore integer,
 EnglishScore integer
 );
+
+# Inserting Data to table students
 Insert into students (studentid, Name, Gender, Age, Grade, Mathscore, ScienceScore, EnglishScore)values
 (1,'Varun', 'M', 15, 'A', 90, 85, 88),
 (2,'Alekhya', 'F', 14, 'B', 75, 80, 72),
@@ -20,33 +25,47 @@ Insert into students (studentid, Name, Gender, Age, Grade, Mathscore, ScienceSco
 (8,'Varshitha', 'F', 15, 'C', 67, 70, 73),
 (9,'Abhishek', 'M', 14, 'D', 50, 55, 60),
 (10,'Soumya', 'F', 16, 'A', 88, 90, 87);
+
+# Displaying all students and their details
 select * from students;
+
+# Calculating the average scores for each subject
 select
 avg(MathScore) AS AvgMathScore,
 avg(ScienceScore) AS AvgScienceScore,
 avg(EnglishScore) AS AvgEnglishScore
 From students;
+
+# Finding the students with the highest total score
 select 
 studentId,
 Name, (MathScore + ScienceScore + EnglishScore) AS TotalScore
 FROM Students
 order by TotalScore desc;
-select
-avg(MathScore) AS AvgMathScore,
-avg(ScienceScore) AS AvgScienceScore,
-avg(EnglishScore) AS AvgEnglishScore
-From students
-GROUP BY Gender;
+
+# Counting the number of students in each grade
 select
 Grade,
 count(*) AS StudentCount
 From students
 group by Grade
 order by Grade;
+
+# Finding the average score for male and female students
+select
+avg(MathScore) AS AvgMathScore,
+avg(ScienceScore) AS AvgScienceScore,
+avg(EnglishScore) AS AvgEnglishScore
+From students
+GROUP BY Gender;
+
+# Identifying students with a Math score above 80
 select
 Name, MathScore
 From students
 where MathScore>80;
+
+# Updating the grade of a student with a specific Student ID
 update students
 set Grade='B'
 where studentID=9;
